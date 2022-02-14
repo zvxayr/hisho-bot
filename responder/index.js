08f9895e80c2a974393094dfd53b0b62cfe7ed6a
zvxayr <carlo4noles@gmail.com>
2022-02-14 21:58:29 +0800

    refactor: variable name changes

--- responder/index.js
+++ responder/index.js
@@ -1,4 +1,4 @@
-import { raise, swallow, compose } from "../utils";
+import { raise } from "../utils";
 
 class CommandNotFound extends Error {
     constructor(command) {
@@ -27,20 +27,22 @@ let alias = {
 
 const decomposeCommand = (full_command) => {
     const format = /^(\S+)?(?:\s+)?(.*)$/s;
-    const [, test_command, full_argument] = format.exec(full_command);
-    return { test_command, full_argument };
+    const [, command, full_argument] = format.exec(full_command);
+    return { command, full_argument };
 }
 
-const resolveAlias = (command) => alias[command] || command;
+const unalias = (command) => {
+    const lower_case_command = command?.toLowerCase();
+    return alias[lower_case_command] ?? lower_case_command;
+}
 
 const findCommand = (command) => command && commands
     .find(({ command: cmd }) => cmd == command);
 
 const responder = (full_command) => {
-    const { test_command, full_argument } = decomposeCommand(full_command);
-    const command = resolveAlias(test_command?.toLowerCase());
-    const { parameters, execute } = findCommand(command)
-        || raise(new CommandNotFound(test_command));
+    const { command, full_argument } = decomposeCommand(full_command);
+    const { parameters, execute } = findCommand(unalias(command))
+        ?? raise(new CommandNotFound(test_command));
     const args = parameters.exec(full_argument).slice(1);
     return execute(...args);
 }
