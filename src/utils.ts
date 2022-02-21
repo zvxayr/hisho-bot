093c527b3b410e811beef296415de5cce30287c0
zvxayr <carlo4noles@gmail.com>
2022-02-22 02:38:19 +0800

    feat: added error handling utility functions

--- src/utils.ts
+++ src/utils.ts
@@ -3,10 +3,32 @@ const raise = (error: Error) => {
     throw error;
 };
 
-type Transformer<Fn extends Function> = (fn: Fn) => Fn;
+const rescue = (error: Error, type: new (...args: any[]) => Error) => {
+    if (!(error instanceof type)) raise(error);
+};
+
+const swallow = (
+    <E extends Error>(type: new (...args: any[]) => E) => (fail: (err: E) => void) => (
+        <Args extends any[]>(fn: (...args: Args) => void) => (...args: Args) => {
+            try {
+                fn(...args);
+            } catch (error: any) {
+                rescue(error, type);
+                fail(error);
+            }
+        }
+    )
+);
+
+type Transformer<Value> = (value: Value) => Value;
 
 const compose = <Inner extends Function>(...fns: Transformer<Inner>[]) => (
     (x: Inner) => fns.reduceRight((v, f) => f(v), x)
 );
 
-export { raise, compose, Transformer };
+export {
+    raise,
+    compose,
+    swallow,
+    Transformer,
+};
